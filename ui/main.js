2cbf0a7f98447d8a2a6a68f9962b0a84912a00f1
anvikram <akilvikram@gmail.com>
2017-03-25 13:02:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,23 @@
 console.log('Loaded!');
-
 var button=document.getElementById('counter');
 button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=count.toString();
+    
+    //Create a request object
+    var request=new XMLHTTPRequest();
+    
+    request.onreadystatechange= function()
+    {
+        if(request.readyState===XMLHTTPRequest.DONE){
+            if(request.status===200){
+                    var counter=request.responseText;
+                 var span=document.getElementById('count');
+                 span.innerHTML=count.toString();
+            }
+        }
+        
+    }
+    
+    request.open('GET','http://anvikram.imad.hasura-app.io/counter',true);
+    request.send(null);
 }
