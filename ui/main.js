4649cca70645799dba9e2bf01238564c59128b8e
anvikram <akilvikram@gmail.com>
2017-03-25 12:55:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 
-var img=document.getElementById("madi");
-img.onclick=function()
+var button=document.getElementById('counter');
+button.onclick=function()
 {
-    img.style.marginLeft='100px';
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=count.toString();
 }
-
