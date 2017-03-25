1d5d0c2c6c97d898ddfe708319fa63a7d3ad3f8a
anvikram <akilvikram@gmail.com>
2017-03-25 11:58:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,16 +102,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleTwo));
-});
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleThree));
-});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
