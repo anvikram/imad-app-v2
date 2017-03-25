205cd42a4d8cb8947c4b52de940d0f3fb1665c11
anvikram <akilvikram@gmail.com>
2017-03-25 20:00:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,7 @@ button.onclick=function()
     request.send(null);
 };
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -48,6 +47,8 @@ submit.onclick = function(){
         
     }
     
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
     request.open('GET','http://anvikram.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
