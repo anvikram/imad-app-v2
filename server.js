7919c04c93fc6a145ac08e04f641a70da38cba7c
anvikram <akilvikram@gmail.com>
2017-03-25 22:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,8 +146,28 @@ app.get('/submit-name', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    //var articleName=req.params.articleName;
+  //res.send(createTemplate(articles[articleName]));
+  
+  pool.query("SELECT * FROM article1 WHERE title = "+req.params.articleName,function(err,result){
+      if(err){
+    res.status(500).send(err.toString());
+}
+else
+{
+    if(result.rows.length===0)
+    {
+        res.status(404).send("Article not found");
+    }
+    else
+    {
+     var articleData=result.rows[0];
+     res.send(createTemplate(articleData));
+    }
+}
+  });
+  
+  
 });
 
 
