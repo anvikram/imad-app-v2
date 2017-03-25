d81356a09a0804d9ed6f9017cd6b81ccd22fe540
anvikram <akilvikram@gmail.com>
2017-03-25 19:46:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/ui/main.js', function (req, res) {
 
 var names=[];
 app.get('/submit-name/:name', function (req, res) {
- var name;
+ var name=req.params.name;
  names.push[name];
  res.send(JSON.stringify(names));
 });
